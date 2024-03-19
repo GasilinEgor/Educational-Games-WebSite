522650da30522e7e8699b7a3601751fbb4852dbb
GasilinEgor <eg.gasilin2018@yandex.ru>
2024-03-19 23:16:17 +0300

    Передача данных с клиента игры на сервер

--- Games/views.py
+++ Games/views.py
@@ -1,4 +1,7 @@
+import json
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
@@ -12,3 +15,15 @@ def game_list(request):
 def speed_score(request):
     context = {'task': "Реши как можно больше задач!"}
     return render(request, 'Games/speedScore.html', context)
+
+
+@csrf_exempt
+def speed(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        print(data)
+        new_value = data.get('value')
+        print(new_value)
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False})
